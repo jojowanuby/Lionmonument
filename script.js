74fec0e4cc1e8df14ea4cc9f3303d2fd12d257f3
andyt <andreas.b.teixeira@gmail.com>
2019-12-08 20:42:35 +0100

    final

--- script.js
+++ script.js
@@ -56,3 +56,19 @@ $('nav a').click(function(event) {
   }, 500);
   event.preventDefault();
 });
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+  if ($('.main_h').hasClass('open-nav')) {
+      $('.main_h').removeClass('open-nav');
+  } else {
+      $('.main_h').addClass('open-nav');
+  }
+});
+
+$('.main_h li a').click(function() {
+  if ($('.main_h').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main_h').removeClass('open-nav');
+  }
+});
